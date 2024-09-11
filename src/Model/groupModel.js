6a8d415771228428bdfc44c968265f2cf3bc3655
Asadullokh <uzbdevit@gmail.com>
2024-09-11 17:06:00 +0500

    group

--- src/Model/groupModel.js
+++ src/Model/groupModel.js
@@ -1,24 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const groupSchema = new mongoose.Schema(
-//   {
-//     weekDay: {
-//       required: true,
-//       enum: ["odd, even"],
-//     },
-//     name:{
-//       type: String,
-//     },
-//     students: {
-//       type: mongoose.Types.ObjectId,
-//       ref: "User",
-//       required: true,
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// module.exports = mongoose.model("Group", groupSchema);
 
 const mongoose = require("mongoose");
 
